ec9bbc95acbc9065731678fdc38e3c1cf0bffe3c
Oleg Arbuzov <boozze@mail.ru>
2019-09-16 16:38:34 +0300

    Верстка полностью

---
+++ source/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var nav = document.querySelector(".navigation");
+var navToggle = document.querySelector(".navigation__toggle");
+
+nav.classList.remove("navigation--nojs");
+
+navToggle.addEventListener("click", function () {
+  if (nav.classList.contains("navigation--closed")) {
+    nav.classList.remove("navigation--closed");
+    nav.classList.add("navigation--opened");
+  } else {
+    nav.classList.add("navigation--closed");
+    nav.classList.remove("navigation--opened");
+  }
+});
